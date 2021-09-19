045e4ae1646250dee86dd7bddba1b5d9834c9e86
kostya-alch <kostafrompiter@gmail.com>
2021-09-19 22:18:49 +0300

    add 404 error page and switch on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { compose } from 'redux';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
 import Navigation from './components/Nav/Navigation';
 import UsersContainer from './components/Users/UsersContainer';
-import { withRouter } from 'react-router';
+import { Redirect, withRouter } from 'react-router';
 import { initialazeApp } from './redux/app-reducer';
 import Preloader from './components/common/Prealoder/Preloader'
 import { WithSuspense } from './hoc/WithSuspense';
@@ -30,16 +30,21 @@ class App extends React.Component {
                <HeaderContainer />
                <Navigation />
                <div className='app-wrapper-content'>
-                  <Route path='/messages'
-                     render={WithSuspense(DialogsContainer)} />
+                  <Switch>
+                     <Route path='/' exact><Redirect to='/profile' /></Route>
+                     <Route path='/messages'
+                        render={WithSuspense(DialogsContainer)} />
 
-                  <Route path='/profiles/:userId?'
-                     render={WithSuspense(ProfileContainer)} />
+                     <Route path='/profiles/:userId?'
+                        render={WithSuspense(ProfileContainer)} />
 
-                  <Route path='/users'
-                     render={() => <UsersContainer />} />
-                  <Route path='/login'
-                     render={() => <Login />} />
+                     <Route path='/users'
+                        render={() => <UsersContainer />} />
+                     <Route path='/login'
+                        render={() => <Login />} />
+                     <Route path='*'
+                        render={() => <div>ERROR 404 NOT FOUND</div>} />
+                  </Switch>
                </div>
             </div>
          </BrowserRouter>
