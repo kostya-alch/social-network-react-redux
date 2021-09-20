0a1d963c84fd44defa861213722a35074c17e95d
kostya-alch <kostafrompiter@gmail.com>
2021-09-20 23:38:23 +0300

    refactor Users.tsx and Paginator.tsx

---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react';
+
+import Paginator from '../common/Paginator/Paginator';
+import { UserType } from '../../types';
+import User from './User';
+
+type PropsType = {
+  totalUsersCount: number;
+  pageSize: number;
+  currentPage: number;
+  onPageChange: (pageNumber: any) => void;
+  followingInProgress: Array<number>;
+  users: Array<UserType>;
+  follow: () => void;
+  unfollow: () => void;
+};
+
+const Users: FC<PropsType> = ({
+  onPageChange,
+  currentPage,
+  totalUsersCount,
+  pageSize,
+  followingInProgress,
+  unfollow,
+  follow,
+  users,
+}) => {
+  return (
+    <div>
+      <Paginator
+        currentPage={currentPage}
+        onPageChange={onPageChange}
+        totalUsersCount={totalUsersCount}
+        pageSize={pageSize}
+      />
+      <div>
+        {users.map((u) => (
+          <User
+            user={u}
+            followingInProgress={followingInProgress}
+            key={u.id}
+            unfollow={unfollow}
+            follow={follow}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Users;
